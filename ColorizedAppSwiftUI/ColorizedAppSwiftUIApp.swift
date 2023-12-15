46bb4c4ee5a83a4180545a1fdef436567094d475
Khafizov <khafizov.den@yandex.ru>
2023-12-16 03:55:27 +0400

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Денис Хафизов on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
